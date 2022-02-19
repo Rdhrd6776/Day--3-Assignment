ecf7405852b7c0c00c6e732ed11a9bdff30ca463
Radheesh R D <99800128+Rdhrd6776@users.noreply.github.com>
2022-02-19 15:46:35 +0530

    Day 3 Assignment

---
+++ Day - 3 Assignment (python).py
@@ -0,0 +1,80 @@
+1. Find the data type of a if a=9
+
+int
+print(type(a))
+
+2. Find the data type of a if a=9
+ 
+ int
+ 
+3. Find the data type of a if a='9.'
+
+str
+
+4. Find the data type of a if a=(9)
+
+int 
+
+5. Find the data type of a if a=False
+
+bool
+
+6. Find the data type of a if a=[1,2,3]
+
+list
+
+7. Find the data type of a if a=(1,2,3)
+
+tuple
+
+8. Find the data type of a if a={'key': 9}
+
+dictionary
+
+9. Find the data type of a if a=1 + 9j
+
+complex
+
+10. Set a=1 and b=2. What data type is a/b?
+
+float
+
+11. Create a dictionary numbers = {'one':1, 'two':2, 'three':3}. Pull out the number '2' by calling the key 'two'
+
+print(numbers['two'])
+
+
+12. Create a tuple with the numbers 8, 9, and 10?
+
+tuple=(8,9,10)
+
+13. Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+d = {one:1, two:2, three:3} d[one]
+
+#error 
+key word must declare in codes if it is a string one ("one","two","three")
+
+#rewrite code 
+
+d={"one":1,"two":2,"three":3}
+print(d["one"])
+
+14 .Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+f = false not f
+
+False and True are boolean data type . first letter of each word start with capital.
+
+f=False 
+
+15. Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+list=[1,2,3]
+list[3]
+
+
+#error 
+out of oder because this list contains only 3 elements and if we try to call for the 4th one it can't [3]
+any of this can be the used to display the nth elements in list  
+ 
+ list[0]
+ list[1]
+ list[2]
